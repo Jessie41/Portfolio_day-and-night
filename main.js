bd8c160d65e3380a1b3ed63d8b3f937ce9221d2e
Jessie41 <56587491+Jessie41@users.noreply.github.com>
2020-08-19 21:53:55 +0100

    Add files via upload
    added while(){}

--- main.js
+++ main.js
@@ -9,10 +9,24 @@ btn.addEventListener('click', (e) => {
     body.style.backgroundColor = 'white';
     body.style.color = 'black';
     btn.textContent = night;
+    const alist = document.querySelectorAll('a');
+
+    let i = 0;
+    while (i < alist.length) {
+      alist[i].style.color = 'green';
+      i++;
+    }
   } else if (btn.textContent === night) {
     body.style.backgroundColor = 'black';
     body.style.color = 'white';
     btn.textContent = day;
+
+    const alist = document.querySelectorAll('a');
+
+    let i = 0;
+    while (i < alist.length) {
+      alist[i].style.color = 'white';
+      i++;
+    }
   }
 });
-nightBtn.addEventListener('click', (e) => {});
